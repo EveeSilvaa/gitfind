1389c75dbe8b84f7d0af2c824c54d075182817ac
evellyn <maevee38@gmail.com>
2024-12-10 16:39:05 -0300

    new config :)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,8 @@
 import { Header } from "../../components/Header";
 import background from "../../assets/background.png";
+import ItemList from "../../components/ItemList";
 import "./styles.css";
+
 
 
 function App() {
@@ -24,6 +26,12 @@ function App() {
             <p>Descrição</p>
           </div>
           <hr />
+          <div>
+            <h4 className="repositorio">Repositórios</h4>
+            <ItemList title="Teste 1" description="teste de descrição" />
+            <ItemList title="Teste 1" description="teste de descrição" />
+            <ItemList title="Teste 1" description="teste de descrição" />
+          </div>
         </div>
       </div>
     </div>
