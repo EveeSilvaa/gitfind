0cb6b5f1fc52ea73030773b0a9cc2b9d51dda1fb
evellyn <maevee38@gmail.com>
2024-12-10 18:31:51 -0300

    new

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
 import background from "../../assets/background.png";
 import ItemList from "../../components/ItemList";
@@ -6,6 +7,27 @@ import "./styles.css";
 
 
 function App() {
+  const { user, setUser} = useState('');
+  const { Currentuser, setCurrentUser} = useState(null);
+  const { repos, setRepos} = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if(newUser.name){
+      const {avatar_url, name, bio} = newUser;
+      setCurrentUser({avatar_url, name, bio});
+
+      const reposData = await fetch(`https://api.github.com/users/${user}`);
+      const newRepos = await reposData.json();
+
+      if(newRepos.length){
+         setRepos(newRepos);
+      }
+    }
+  };
+
   return (
     <div className="App">
       <Header />
@@ -13,10 +35,15 @@ function App() {
         <img src={background} className="background" alt="backgroud app" />
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@username" />
-            <button>Buscar</button>
+            <input name="usuario" 
+            value={user} 
+            onChange={(event) => setUser(event.target.value)} 
+            placeholder="@username" />
+            <button onClick={handleGetData}>Buscar</button>
           </div>
-          <div className="perfil">
+          {currentUser?.name ? (
+            <>
+            <div className="perfil">
             <img src="src/assets/134736070.jpg" 
             className="profile" alt="imagem de perfil" />
           </div>
@@ -26,12 +53,16 @@ function App() {
             <p>Descrição</p>
           </div>
           <hr />
+          </>
+          ): null}
+          {repos?.length ? (
           <div>
             <h4 className="repositorio">Repositórios</h4>
             <ItemList title="Teste 1" description="teste de descrição" />
             <ItemList title="Teste 1" description="teste de descrição" />
             <ItemList title="Teste 1" description="teste de descrição" />
           </div>
+          ) : null}
         </div>
       </div>
     </div>
